7dfd314c4c437dd50926602711d65040b251b85d
Eric Rincon <royeric77@gmail.com>
2022-12-24 19:35:01 -0600

    draft

--- parking.py
+++ parking.py
@@ -7,19 +7,36 @@ class Garage():
 
 
     def __init__(self):
-        tickets = []
-        parkingSpaces = []
-        currentTicket = {}
+        self.tickets = []
+        self.parkingSpaces = []
+        self.currentTicket = {}
 
     def TakeTicket(self):
-
+        if ticket in self.tickets:
+            self.tickets - 1
+        else:
+            print("You have no tickets to give.")
+        
+        self.parkingSpaces - 1
     
-    def payForParking(self):
+    def payForParking(self, pay):
+        
+        payment = input("How much would you like to pay?")
+        
+        if pay in payment:
+            print("Thank you for paying! You have 15 minutes to exit the garage.")
+            currentTicket(paid) = True
 
     
 
     def leaveGarage(self):
+        if pay not in payment:
+            print("We have not yet recieved payment.")
+            self.payForParking(pay)
+            
+        elif pay in payment:
+            print("Thank you, have a nice day!")
 
+            self.parkingSpaces + 1
+            self.tickets + 1
 
-
-    def 
