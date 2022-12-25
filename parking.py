5d5afd999419f4e1b25001765bd25f367a568418
Eric Rincon <royeric77@gmail.com>
2022-12-24 19:09:33 -0600

    where we wil start from

---
+++ parking.py
@@ -0,0 +1,25 @@
+#This can be a little flexible
+# 3 Methods: takeTicket, parForParking, leaveGarage
+# few attributes: tickets - list, parkingSpaces - list, currentTicket - dictionary
+
+
+class Garage():
+
+
+    def __init__(self):
+        tickets = []
+        parkingSpaces = []
+        currentTicket = {}
+
+    def TakeTicket(self):
+
+    
+    def payForParking(self):
+
+    
+
+    def leaveGarage(self):
+
+
+
+    def 
