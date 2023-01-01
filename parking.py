44ebb7c6415098c2d7bd43a61e03cb5419634656
Eric Rincon <royeric77@gmail.com>
2023-01-01 17:30:47 -0600

    almost final

--- parking.py
+++ parking.py
@@ -6,37 +6,64 @@
 class Garage():
 
 
-    def __init__(self):
-        self.tickets = []
-        self.parkingSpaces = []
+    def __init__(self, tickets, parkingSpaces):
+        self.tickets = tickets
+        self.parkingSpaces = parkingSpaces
         self.currentTicket = {}
 
     def TakeTicket(self):
-        if ticket in self.tickets:
-            self.tickets - 1
+        if self.parkingSpaces <= 0:
+            print("Sorry, there is no parking space left.")
+            return
+        
+        if self.tickets > 0:
+            self.tickets -= 1
         else:
             print("You have no tickets to give.")
+            
+        self.parkingSpaces -=1
         
-        self.parkingSpaces - 1
+
+       
     
-    def payForParking(self, pay):
+    def payForParking(self, pay=None):
         
-        payment = input("How much would you like to pay?")
-        
-        if pay in payment:
+        pay = input("How much would you like to pay?: ")
+
+        if int(pay) > 0:
+            self.currentTicket['paid'] = True
             print("Thank you for paying! You have 15 minutes to exit the garage.")
-            currentTicket(paid) = True
+
+        elif pay is None:
+            print("We have not yet recieved payment.")
+
+        elif int(pay) <= 0:
+            print("We have not yet recieved payment.")
 
     
 
     def leaveGarage(self):
-        if pay not in payment:
-            print("We have not yet recieved payment.")
-            self.payForParking(pay)
-            
-        elif pay in payment:
+        if self.currentTicket.get('paid'):
             print("Thank you, have a nice day!")
 
-            self.parkingSpaces + 1
-            self.tickets + 1
+            
+            
+        else:
+            pay = input("Please enter payment: ")
+            
+            while int(pay) <= 0:
+                pay = input("Please enter a payment of 1 or more: ")
+            else:
+               print("Thank you, have a nice day!")
+               
+       
+        self.parkingSpaces += 1
+        self.tickets += 1    
+
+
+garage = Garage(10,0)
+garage.TakeTicket()
+garage.payForParking()
+garage.leaveGarage()
+
 
