5d0234cd35e8983af7970580834033033a21bd40
Eric Rincon <royeric77@gmail.com>
2023-01-02 14:47:22 -0600

    Final project after working with brandt on something

--- parking.py
+++ parking.py
@@ -5,18 +5,16 @@
 
 class Garage():
 
-
-    def __init__(self, tickets, parkingSpaces):
-        self.tickets = tickets
-        self.parkingSpaces = parkingSpaces
+    def __init__(self):
+        self.tickets = 10
+        self.parkingSpaces = 5
         self.currentTicket = {}
 
     def TakeTicket(self):
         if self.parkingSpaces <= 0:
             print("Sorry, there is no parking space left.")
             return
-        
-        if self.tickets > 0:
+        elif self.tickets > 0:
             self.tickets -= 1
         else:
             print("You have no tickets to give.")
@@ -45,6 +43,9 @@ class Garage():
     def leaveGarage(self):
         if self.currentTicket.get('paid'):
             print("Thank you, have a nice day!")
+            self.parkingSpaces += 1               
+            self.tickets += 1 
+            return
 
             
             
@@ -55,15 +56,31 @@ class Garage():
                 pay = input("Please enter a payment of 1 or more: ")
             else:
                print("Thank you, have a nice day!")
+               self.parkingSpaces += 1               
+               self.tickets += 1 
+               return
                
-       
-        self.parkingSpaces += 1
-        self.tickets += 1    
+            
+
+    def run(self):
+        if self.parkingSpaces <= 0:
+            print("Sorry, there is no parking available.")
+            return
+        while True:
+            x = input("Welcome to the parking garage! What do you want to do? GiveTicket/PayParking/Leave: ")
+            if x.lower() == "leave":
+                self.leaveGarage()
+                break
+            elif x.lower() == "giveticket":
+                self.TakeTicket()
+            elif x.lower() == "payparking":
+                self.payForParking()
+                break
+            else:
+                print("Invalid option, please type GiveTicket/PayParking/Leave (not case sensitive)")
 
 
-garage = Garage(10,0)
-garage.TakeTicket()
-garage.payForParking()
-garage.leaveGarage()
+garage = Garage()
+garage.run()
 
 
